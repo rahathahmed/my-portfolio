917914082957453e2b480e328a56fc9481f74ec7
rahathahmed <rahathpersonal@gmail.com>
2021-07-05 12:52:20 +0600

    first commit

---
+++ asset/js/custom.js
@@ -0,0 +1,79 @@
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+$(function(){
+
+  $('.count-num').rCounter();
+
+});
+
+$(document).ready(function(){
+    $('.progressbar1').LineProgressbar();
+    percentage: 74
+});
+$('.progressbar2').LineProgressbar({
+    percentage: 65
+});
+$('.progressbar3').LineProgressbar({
+    percentage: 80
+});
+$('.progressbar4').LineProgressbar({
+    percentage: 75
+});
+
+$('.progressbar5').LineProgressbar({
+    percentage: 90
+});
+
+$('.counter').counterUp({
+    delay: 30,
+    time: 7000
+});
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+window.scroll({
+  top: 2500, 
+  left: 0, 
+  behavior: 'smooth'
+});
+
+// Scroll certain amounts from current position 
+window.scrollBy({ 
+  top: 100, // could be negative value
+  left: 0, 
+  behavior: 'smooth' 
+});
+
+// Scroll to a certain element
+document.querySelector('.hello').scrollIntoView({ 
+  behavior: 'smooth' 
+});
+
+
+
